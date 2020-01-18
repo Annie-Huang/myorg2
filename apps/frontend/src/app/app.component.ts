2731dc0cc210931ad858ba4c8d8e7cbc980d1a43
Annie Huang <annie.hn@gmail.com>
2020-01-18 14:03:18 +1100

    Update app.app.component.ts to call api

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@myorg2/api-interfaces';
+import {Observable} from 'rxjs';
+
+interface Todo {
+  title: string;
+}
 
 @Component({
   selector: 'myorg2-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+    <h1>Todos</h1>
+    <ul>
+      <li *ngFor="let t of todos | async">{{ t.title }}</li>
+    </ul>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  // hello$ = this.http.get<Message>('/api/hello');
+
+  todos: Observable<Todo[]>;
+
+  constructor(private http: HttpClient) {
+    this.todos = http.get<Todo[]>('/api/todos');
+  }
 }
